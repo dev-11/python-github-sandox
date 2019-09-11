eae65f513c2a8a17d39bd5f61a3b3eb248be7e43
Otto <otto@masterbranch.io>
2019-09-11 15:09:22 +0100

    :poop: adding code smell to test sonarcloud

--- app.py
+++ app.py
@@ -8,6 +8,15 @@ def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
 
+def find(seq, target):
+    found = False
+    for i, value in enumerate(seq):
+        if value == target:
+            found = True
+            break
+    if not found:
+        return -1
+    return i
 
 if __name__ == '__main__':
     app.run(debug=True, port=5006)
