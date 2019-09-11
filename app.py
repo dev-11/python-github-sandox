c0df4c525440e96d8aa38f0523f750e071ddab34
Otto <otto@masterbranch.io>
2019-09-11 10:35:18 +0100

    :tada: Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, make_response, escape
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5006)
