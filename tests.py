43a700490d1c7b518274b40819a8cc5d96653c7b
Otto <otto@masterbranch.io>
2019-09-20 22:53:01 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-target = __import__("my_app")
+import my_app
 
 def test_normal():
     pass
@@ -8,7 +8,7 @@ def test_normal_2():
     pass
 
 def test_3():
-    a = target.app.fun(1)
+    a = my_app.fun(1)
     assert a == 2
     
     
